40b5649d6a94bbb5c5c775fae4fe8300d558481a
Ludovic <lfavier@live.com>
2019-12-25 22:06:53 +0100

    Amélioration code pour trouver version d'une app + on ignore les fichiers objets et exécutables

--- Daemon/find_app.h
+++ Daemon/find_app.h
@@ -1,1 +1,1 @@
-void find_app(char *path_app);
+char * find_app(char *path_app);
--- Daemon/main.c
+++ Daemon/main.c
@@ -13,6 +13,7 @@
 int main(int argc, char **argv)
 {
     char *path_app = "C:\\Riot Games\\League of Legends\\LeagueClient.exe";  //chemin d'accès d'une application qu'on veut identifier sa version
+    char * version_app;
 
     MYSQL *connection = my_MySQL(); //appel de fonction pour se connecter à MySQL
 
@@ -20,7 +21,11 @@ int main(int argc, char **argv)
 
     /* Application locale */
     printf("\nYour software: %s", path_app);
-    find_app(path_app);
+    version_app = find_app(path_app);
+    if (version_app != NULL) {
+            printf("\nYour version: %s", version_app);
+            free(version_app);
+    }
 
 
     mysql_close(connection); //avant d'arrêter le programme, on arrête la connexion avec MySQL
